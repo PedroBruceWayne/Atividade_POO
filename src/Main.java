a4325a3591bc60d56898d65d791002d0a3938b31
PedroBruceWayne <pedro.ffj@hotmail.com>
2025-05-16 16:49:45 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== SISTEMA DE CADASTRO DE INSTRUTORES ===");
+        InstrutorController controller = new InstrutorController();
+        controller.iniciar();
+    }
+}
